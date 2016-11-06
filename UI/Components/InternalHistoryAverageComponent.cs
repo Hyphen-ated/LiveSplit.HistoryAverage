47607e944e8b42444e6dbaa5a7eeec33b9851b3d
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2016-11-06 14:40:35 -0800

    copied the Text component while renaming everything to HistoryAverage

---
+++ UI/Components/InternalHistoryAverageComponent.cs
@@ -0,0 +1,20 @@
+﻿namespace LiveSplit.UI.Components
+{
+    public class InternalHistoryAverageComponent : InfoTextComponent
+    {
+        public HistoryAverageComponentSettings Settings { get; set; }
+
+        public InternalHistoryAverageComponent(HistoryAverageComponentSettings settings)
+            : base("", "")
+        {
+            Settings = settings;
+        }
+
+        public override void PrepareDraw(Model.LiveSplitState state, LayoutMode mode)
+        {
+            NameMeasureLabel.Font = Settings.OverrideFont1 ? Settings.Font1 : state.LayoutSettings.TextFont;
+            ValueLabel.Font = Settings.OverrideFont2 ? Settings.Font2 : state.LayoutSettings.TextFont;
+            NameLabel.Font = Settings.OverrideFont1 ? Settings.Font1 : state.LayoutSettings.TextFont;
+        }
+    }
+}
---
+++ UI/Components/TextComponentFactory.cs
@@ -0,0 +1,24 @@
+﻿using LiveSplit.Model;
+using System;
+
+namespace LiveSplit.UI.Components
+{
+    public class HistoryAverageComponentFactory : IComponentFactory
+    {
+        public string ComponentName => "History Average";
+
+        public string Description => "Calculates average run times from your history.";
+
+        public ComponentCategory Category => ComponentCategory.Information;
+
+        public IComponent Create(LiveSplitState state) => new HistoryAverageComponent(state);
+
+        public string UpdateName => ComponentName;
+
+        public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.HistoryAverage.xml";
+
+        public string UpdateURL => "http://livesplit.org/update/";
+
+        public Version Version => Version.Parse("0.0.1");
+    }
+}
